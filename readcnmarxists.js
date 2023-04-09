0d6ef1b9f991df07e22e134584113b94f38d55c7
ZCQ1111 <zcqmtq@gmail.com>
2023-04-09 23:02:46 +0800

    Create readcnmarxists.js

---
+++ readcnmarxists.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         中马库阅读脚本
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.marxists.org/chinese/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=marxists.org
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const css = `
+body {
+    MARGIN-LEFT: 23em !important;
+    MARGIN-RIGHT: 20em !important;
+    background: none;
+}
+table {
+display: initial;
+    margin-left: 0 !important;
+    margin-right: 0 !important;
+}
+div, td {
+    background: none !important;
+}
+`
+GM_addStyle(`
+${css}
+`);
+    // Your code here...
+})();
